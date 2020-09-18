64f8f71d77ffc25166a121bf83f6bb9cdfe44405
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-18 20:39:35 +0100

    adding and reading api keys from file

--- app/src/main/java/agh/hafid/news/MainActivity.kt
+++ app/src/main/java/agh/hafid/news/MainActivity.kt
@@ -2,8 +2,10 @@ package agh.hafid.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
