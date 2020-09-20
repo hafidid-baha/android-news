830e01c4977934d951b5a93a60842bf5e18e4c6d
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-20 18:53:09 +0100

    fetch data from api

--- app/src/main/java/agh/hafid/news/NewsFragment/NewsFragment.kt
+++ app/src/main/java/agh/hafid/news/NewsFragment/NewsFragment.kt
@@ -26,7 +26,8 @@ class NewsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(NewsViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.fetchNews()
     }
 
 }
--- app/src/main/java/agh/hafid/news/NewsFragment/NewsViewModel.kt
+++ app/src/main/java/agh/hafid/news/NewsFragment/NewsViewModel.kt
@@ -1,7 +1,33 @@
 package agh.hafid.news.NewsFragment
 
+import agh.hafid.news.R
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
-class NewsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class NewsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private lateinit var news:LiveData<List<String>>
+    private var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=%s"
+    private var queue = Volley.newRequestQueue(application)
+    private val app = application
+
+    fun fetchNews(){
+        val key = app.getString(R.string.API_KEY)
+        val request = StringRequest(Request.Method.GET, url.format(key),
+            Response.Listener { response ->
+            Log.d("data", "fetchNews: Response is: ${response.substring(0, 500)}")
+        },
+            Response.ErrorListener { Log.d("data", "fetchNews: error") }
+        )
+
+        queue.add(request)
+    }
+
 }
