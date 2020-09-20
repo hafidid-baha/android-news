8f7d7ec3942706b7107ce625f7924b9ca63b7280
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-20 18:24:36 +0100

    add navigation code to news fragment

--- app/src/main/java/agh/hafid/news/MainFragment/MainFragment.kt
+++ app/src/main/java/agh/hafid/news/MainFragment/MainFragment.kt
@@ -1,15 +1,20 @@
 package agh.hafid.news.MainFragment
 
+import agh.hafid.news.MainActivity
 import agh.hafid.news.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
+    lateinit var ReadNewsBtn:Button
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -20,13 +25,18 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view =  inflater.inflate(R.layout.main_fragment, container, false)
+        ReadNewsBtn = view.findViewById(R.id.readNews)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        ReadNewsBtn.setOnClickListener { view ->
+            viewModel.navigateToNews(view)
+        }
     }
 
 }
--- app/src/main/java/agh/hafid/news/MainFragment/MainViewModel.kt
+++ app/src/main/java/agh/hafid/news/MainFragment/MainViewModel.kt
@@ -1,7 +1,15 @@
 package agh.hafid.news.MainFragment
 
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    val app:Application = application
+    fun navigateToNews(v:View){
+        val action = MainFragmentDirections.actionMainFragmentToNewsFragment()
+        v.findNavController().navigate(action)
+    }
 }
