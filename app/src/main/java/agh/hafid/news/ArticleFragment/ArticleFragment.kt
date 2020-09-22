ec762a09f683e210c4e82aa3fdecac95ebd46752
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-22 17:35:06 +0100

    add button to navigate to the artcile content

--- app/src/main/java/agh/hafid/news/ArticleFragment/ArticleFragment.kt
+++ app/src/main/java/agh/hafid/news/ArticleFragment/ArticleFragment.kt
@@ -1,16 +1,19 @@
 package agh.hafid.news.ArticleFragment
 
+import agh.hafid.news.NewsFragment.NewsFragmentDirections
 import agh.hafid.news.R
 import android.icu.text.CaseMap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.squareup.picasso.Picasso
 
@@ -25,6 +28,7 @@ class ArticleFragment : Fragment() {
     lateinit var title: TextView
     lateinit var date: TextView
     lateinit var content: TextView
+    lateinit var btnReadMore: Button
     val args: ArticleFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,18 +39,23 @@ class ArticleFragment : Fragment() {
         title = view.findViewById(R.id.articleTitle)
         date = view.findViewById(R.id.articleDate)
         content = view.findViewById(R.id.articleContent)
+        btnReadMore = view.findViewById(R.id.readMore)
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
-        // TODO: Use the ViewModel
 
         Picasso.get().load(args.image).into(image)
         title.text = args.titile
         content.text = args.content
         date.text = args.date
+
+        btnReadMore.setOnClickListener {
+            val action = ArticleFragmentDirections.actionArticleFragmentToContentFragment(args.article)
+            it.findNavController().navigate(action)
+        }
     }
 
 }
