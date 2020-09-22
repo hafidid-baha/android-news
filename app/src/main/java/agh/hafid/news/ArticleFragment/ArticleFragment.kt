8d5b0b05865d8cb2fe68bd68b57b2fe55f89d02b
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-22 14:42:39 +0100

    create article fragment

---
+++ app/src/main/java/agh/hafid/news/ArticleFragment/ArticleFragment.kt
@@ -0,0 +1,32 @@
+package agh.hafid.news.ArticleFragment
+
+import agh.hafid.news.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+class ArticleFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ArticleFragment()
+    }
+
+    private lateinit var viewModel: ArticleViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.article_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/agh/hafid/news/ArticleFragment/ArticleViewModel.kt
@@ -0,0 +1,7 @@
+package agh.hafid.news.ArticleFragment
+
+import androidx.lifecycle.ViewModel
+
+class ArticleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
